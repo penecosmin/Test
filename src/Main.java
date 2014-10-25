5b543e8418d48f995caf60dcf2e3eb6dc51ed591
Fulgeanu <full.ioann@gmail.com>
2014-10-25 16:06:35 +0300

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+	public static void main(String[] args){
+		Persoana unu = new Persoana("Dorel", "Gigi", 23);
+		unu.setNume("Alex");
+		System.out.println(unu.getNume());
+		System.out.println(unu.getPrenume());
+		System.out.println(unu.getVarsta());
+	}
+
+		
+}
